fa693cd4f5cda179439d3173bbde7324bd21ff7f
chaewonkong <chaewonkong@gmail.com>
2022-06-13 20:58:24 +0900

    feat: 🎸 MST-크루스칼 알고리즘

--- algorithm-with-java/src/graph/ConnectNetwork.java
+++ algorithm-with-java/src/graph/ConnectNetwork.java
@@ -1,50 +1,97 @@
 package graph;
 
-import java.util.Scanner;
-
-// TODO: MST, 크루스칼 알고리즘
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class ConnectNetwork {
-    private static int[][] arr;
-    private static boolean[] isVisited;
-    private static int M, N, cost;
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        M = sc.nextInt();
-        arr = new int[N][N];
-        int minCost = Integer.MAX_VALUE;
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int ans = 0;
+
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+
+        Edge[] network = new Edge[M];
+        int[] parent = new int[N + 1];
+
+        // parent 배열을 초기화
+        for (int i = 0; i <= N; i++) {
+            parent[i] = i;
+        }
+
+        // 정점과 간선 입력을 network에 저장
         for (int i = 0; i < M; i++) {
-            int a = sc.nextInt() -1;
-            int b = sc.nextInt() -1;
-            int cost = sc.nextInt();
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
 
-            arr[a][b] = cost;
-            arr[b][a] = cost;
+            network[i] = new Edge(start, end, weight);
         }
 
-        for (int i = 0; i < N; i++) {
-            isVisited = new boolean[N];
-            cost = 0;
-            dfs(i);
-            minCost = Math.min(cost, minCost);
-        }
+        br.close();
 
-        System.out.println(minCost);
-    }
+        Arrays.sort(network);
+        for (int i = 0; i < M; i++) {
+            Edge edge = network[i];
 
-    private static void dfs(int idx){
-        // 탈출조건
-        isVisited[idx] = true;
-
-        for (int i = 0; i < N; i++) {
-            if (i != idx && !isVisited[i] && arr[idx][i] > 0) {
-                cost += arr[idx][i];
-                dfs(i);
+            if (!hasSameParent(parent, edge.start, edge.end)) { // 사이클이 발생하지 않는다면
+                // 간선을 연결하고 비용을 가산
+                union(parent, edge.start, edge.end);
+                ans += edge.weight;
             }
         }
 
-        // for문으로 dfs 재귀호출
+        System.out.println(ans);
+    }
+
+    private static class Edge implements Comparable<Edge> {
+        int start;
+        int end;
+        int weight;
+
+        public Edge(int start, int end, int weight) {
+            this.start = start;
+            this.end = end;
+            this.weight = weight;
+        }
+
+        @Override
+        public int compareTo(Edge o) {
+            return weight - o.weight;
+        }
+    }
+
+    private static int getParent(int[] parent, int x) {
+        if (parent[x] == x) {
+            return x;
+        }
+
+        parent[x] = getParent(parent, parent[x]);
+        return parent[x];
+    }
+
+    // 두 부모를 합치는 함수. 더 작은 부모로 합친다.
+    private static void union(int[] parent, int x, int y) {
+        x = getParent(parent, x);
+        y = getParent(parent, y);
+
+        if (x < y) {
+            parent[y] = x;
+        } else {
+            parent[x] = y;
+        }
+    }
+
+    // 같은 부모를 가지는지 확인
+    private static boolean hasSameParent(int[] parent, int x, int y) {
+        x = getParent(parent, x);
+        y = getParent(parent, y);
+        return x == y;
     }
 }
